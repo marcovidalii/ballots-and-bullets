bfa7803d6341b91cf1a4795cee0f268b9327801b
marcovidalii <124085956+marcovidalii@users.noreply.github.com>
2023-02-21 19:16:48 +0100

    Done: Footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
     import "../app.css";
 </script>
 
-<slot />
+<div class="h-full lg:flex lg:flex-col">
+    <slot />
+    <footer class="footer footer-center p-4 bg-primary text-primary-content">
+        <div>
+            <p>Copyright © 2023 - All right reserved by Marco Vidali & Co.</p>
+        </div>
+    </footer>
+</div>
