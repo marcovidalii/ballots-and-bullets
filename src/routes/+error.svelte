b4462b3c7612aff2399e365c8e3e0d5c1352a273
marcovidalii <124085956+marcovidalii@users.noreply.github.com>
2023-02-22 12:32:46 +0100

    Done: 404 error

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<div class="flex justify-center items-center h-full">
+    <div class="flex items-center gap-3 lg:flex-row flex-col">
+        <p class="text-primary responsive-text-8xl font-bold">404</p>
+        <p class="text-primary responsive-text-4xl font-bold lg:block hidden">
+            -
+        </p>
+        <p class="text-primary responsive-text-4xl font-bold">not found</p>
+    </div>
+</div>
