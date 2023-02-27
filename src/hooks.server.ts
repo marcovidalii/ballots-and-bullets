2d849a483740e272128e5e58c84dd410e5c5ccaf
marcovidalii <124085956+marcovidalii@users.noreply.github.com>
2023-02-27 22:27:05 +0100

    Done: Traffic routing

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -17,22 +17,83 @@ export const handle: Handle = async ({ event, resolve }) => {
         }
 
         // user is signed in and isn't in game
-        let { data } = await event.locals.sb
-            .from("profiles")
-            .select("game")
-            .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id);
-        if (!data![0].game) {
+        let isInGame = (
+            await event.locals.sb
+                .from("profiles")
+                .select("game")
+                .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id)
+        ).data![0].game;
+        if (!isInGame) {
             throw redirect(303, "/join-or-create");
+        }
+
+        // user is in game and it isn't started
+        const currentGame = (
+            await event.locals.sb
+                .from("profiles")
+                .select("game")
+                .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id)
+        ).data![0].game;
+        const isGameStarted = (
+            await event.locals.sb
+                .from("games")
+                .select("is_started")
+                .eq("id", currentGame)
+        ).data![0].is_started;
+        if (!isGameStarted) {
+            throw redirect(303, "/lobby");
         }
     }
 
     if (event.url.pathname === "/join-or-create") {
+        // user isn't signed in
+        if (!(await event.locals.sb.auth.getUser()).data.user) {
+            throw redirect(303, "/");
+        }
+
         // user is already in a game
-        let { data } = await event.locals.sb
-            .from("profiles")
-            .select("game")
-            .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id);
-        if (data![0].game) {
+        let isInGame = (
+            await event.locals.sb
+                .from("profiles")
+                .select("game")
+                .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id)
+        ).data![0].game;
+        if (isInGame) {
+            throw redirect(303, "/");
+        }
+    }
+
+    if (event.url.pathname === "/lobby") {
+        // user isn't signed in
+        if (!(await event.locals.sb.auth.getUser()).data.user) {
+            throw redirect(303, "/");
+        }
+
+        // user isn't in game
+        let isInGame = (
+            await event.locals.sb
+                .from("profiles")
+                .select("game")
+                .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id)
+        ).data![0].game;
+        if (!isInGame) {
+            throw redirect(303, "/");
+        }
+
+        // user is in game and it is started
+        const currentGame = (
+            await event.locals.sb
+                .from("profiles")
+                .select("game")
+                .eq("id", (await event.locals.sb.auth.getUser()).data.user!.id)
+        ).data![0].game;
+        const isGameStarted = (
+            await event.locals.sb
+                .from("games")
+                .select("is_started")
+                .eq("id", currentGame)
+        ).data![0].is_started;
+        if (isGameStarted) {
             throw redirect(303, "/");
         }
     }
